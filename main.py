b83110b520a41207c58a3dc7f3e8dcabe63d8934
Muhammad Harits R <harits.muhammad.only@gmail.com>
2024-07-26 17:03:28 +0700

    refine detail part

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Ferrscrape:
 
     def webdriversetup(self):
         ff_opt = Options()
-        # ff_opt.add_argument('-headless')
+        ff_opt.add_argument('-headless')
         ff_opt.add_argument('--no-sandbox')
         ff_opt.set_preference("general.useragent.override", self.user_agent)
         ff_opt.page_load_strategy = 'eager'
@@ -53,7 +53,8 @@ class Ferrscrape:
     def fetch(self, endpoint):
         url = urljoin(scraper.base_url, endpoint)
         headers = {
-            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64)'
+            # 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64)'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0'
         }
 
         with Client(headers=headers) as client:
@@ -61,7 +62,6 @@ class Ferrscrape:
             response = client.get(url)
         if response.status_code != 200:
             response.raise_for_status()
-        # print(response)
 
         return response
 
@@ -71,20 +71,19 @@ class Ferrscrape:
         part_elems = tree.css('div.parts-table > div.row')
         for elem in part_elems:
             description_elem = elem.css_first('div.col-12')
-            detail_part_elems = description_elem.css('a.part-name')
+            detail_part_elems = description_elem.css('div.row.bg-box > div.col-12')
             for detail_part_elem in detail_part_elems:
                 data = dict()
                 data['#'] = elem.css_first('div.col-3').text(strip=True)
                 data['part_number'] = elem.css_first('div.col-9').text(strip=True)
-                data['description'] = description_elem.text(strip=True)
-                data['detail'] = detail_part_elem.text(strip=True)
+                data['description'] = elem.css_first('div.col-12').text(strip=True)
                 try:
                     data['key'] = elem.css_first('button.btn.btn-success.btn-icon.btn-sm').text(strip=True)
                 except:
-                    data['key'] = ''
-                data['price'] = elem.css_first('div.d-block').text(strip=True)
+                    data['key'] = elem.css_first('button.btn.btn-outline-secondary.btn-icon.btn-sm')
+                data['detail'] = detail_part_elem.css_first('a.part-name').text(strip=True)
+                data['price'] = detail_part_elem.css_first('span.cart-price-animation').text(strip=True)
                 datas.append(data)
-                # print(data)
 
         return datas
 
